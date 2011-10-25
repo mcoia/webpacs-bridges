d34aa9bfa53982cac3a94f0c55be9fbcff67ee52
hammondjn <jessica@mobiusconsortium.org>
2011-10-25 14:26:56 -0500

    Set up the texting feature

--- sms.js
+++ sms.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 // set this to be the URL for the SMS script
-var smsurl = "http://mobiusconsortium.org/sms/sms-training.php?";
+var smsurl = "http://mobiusconsortium.org/sms/sms-bridges.php?";
 
    function showsms() {
 
